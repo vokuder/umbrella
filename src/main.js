1e99e0a38e992979119e91d70fa5d6cd7bbd98ce
Jan-Michael Löhlein <jan-michael.loehlein@hs-hannover.de>
2021-02-22 20:45:51 +0100

    Add vuetify

--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import vuetify from '@/components/vuetify'
+import 'roboto-fontface/css/roboto/roboto-fontface.css'
+import '@fortawesome/fontawesome-free/css/all.css'
 
 
 Vue.config.productionTip = false
@@ -10,5 +13,6 @@ Vue.config.productionTip = false
 new Vue({
   router,
   store,
+  vuetify,
   render: h => h(App)
 }).$mount('#app')
