58f88a089a895d86f4ef7ce814a41ad56ad9e211
Jan-Michael Löhlein <jan-michael.loehlein@hs-hannover.de>
2021-02-24 17:23:41 +0100

    Remove unused code

--- src/views/WeatherView.vue
+++ src/views/WeatherView.vue
@@ -17,23 +17,6 @@
 
         <CurrentWeatherCard></CurrentWeatherCard>
       </v-container>
-
-      <!--
-      <v-carousel cycle :interval="2000" :continuous="true" :show-arrows="false" hide-delimiters>
-        <v-carousel-item v-for="(day,i) in forecastDays" :key="i">
-          <v-container>
-            <v-card>
-              <v-card-title>{{day.title}}</v-card-title>
-              <v-card-text>
-                <h1 class="mb-5">{{day.temp}}°C</h1>
-                <h1 class="subtitle-1">Humidity: {{day.humidity}} %</h1>
-                <h1 class="subtitle-1">Wind: {{day.windSpeed}} km/h</h1>
-              </v-card-text>
-            </v-card>
-          </v-container>
-        </v-carousel-item>
-      </v-carousel>
-    -->
     </v-main>
   </v-app>
 </template>
